a4febf5af219b4dfdf70be373e20751446fec2ec
Edilma Riano <edilmaria@hotmail.com>
2023-02-17 11:58:57 -0500

    COMPLETED the appi app. Tested and Working

--- functions/src/task.js
+++ functions/src/task.js
@@ -1,8 +1,8 @@
 import { getFirestoreInstance } from "./utils.js";
 import { FieldValue } from "firebase-admin/firestore";
 
-export function getAllTasks(req, res){
-    const db = getFirestoreInstance();
+export async function getAllTasks(req, res){
+    const db = await getFirestoreInstance();
     console.log("Got connected")
 
     db.collection("tasks").orderBy("createdAt","desc").get()
@@ -27,10 +27,14 @@ export async function addTask(req,res){
         .catch(err => res.status(500).send({error: err.message}))
 }
 
+// update a task
+
 export async function updateTask (req,res){
     const {taskId} = req.params;
     const {done} = req.body
+    //console.log("before")
     const db = await getFirestoreInstance();
+    console.log('connected to db')
     db.collection("tasks")
         .doc(taskId)
         .update({done})
@@ -38,6 +42,7 @@ export async function updateTask (req,res){
         .catch(err => res.status(500).send({error: err.message}))
 }
 
+//delete a task
 export async function deleteTask(req, res){
 
     const {taskId} = req.params
